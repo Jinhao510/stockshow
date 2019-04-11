18f2f8faffb55071380b2313fbb1018c10733350
Jinhao510 <37235523+Jinhao510@users.noreply.github.com>
2019-04-11 14:50:41 +0800

    v0.5

---
+++ temp.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+20190411
+my sql test use 2317 stock data 
+
+"""
+import pymysql
+
+db = pymysql.connect(host='localhost', port=3306, user='root', passwd='bb556656', db='my_db', charset='utf8')
+#建立操作游標
+cursor = db.cursor()
+#SQL語法（查詢資料庫版本）
+sql = 'select hei from com_per'
+
+#執行語法
+cursor.execute(sql)
+#選取第一筆結果
+data = cursor.fetchone()
+
+print ("Database version : %s " % data)
+
+#關閉連線
+db.close()
