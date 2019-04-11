536b8afe3112fd4a6c83bc61a54f911be179432a
Jinhao510 <37235523+Jinhao510@users.noreply.github.com>
2019-04-11 18:00:22 +0800

    0411fin

--- xlstomysql.py
+++ xlstomysql.py
@@ -9,7 +9,15 @@ import pandas as pd
 import pymysql
 
 db = pymysql.connect(host='localhost', port=3306, user='root', passwd='bb556656', db='my_db', charset='utf8')
+cursor = db.cursor()
+sql = 'select wei from com_per'
+cursor.execute(sql)
+data = cursor.fetchone()
+print ("one data: %s " % data)
+
 
 df = pd.read_excel("2317BSD.xlsx")
 d=(df.head(20))
 print (d)
+
+db.close()
